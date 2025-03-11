25bdea04b49a4e5bccc1c6b184dee38ce4061c26
root <root@LAPTOP-FTAK1K8O.>
2025-03-11 10:23:35 +0800

    0311 update

---
+++ run_v0.py
@@ -0,0 +1,24 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+
+model_path = "liuhaotian/llava-v1.5-7b"
+
+prompt = "請解釋這個表格的內容並分析"
+image_file = "table/2.jpg"
+
+args = type('Args', (), {
+    "model_path": model_path,
+    "model_base": None,
+    "model_name": get_model_name_from_path(model_path),
+    "query": prompt,
+    "conv_mode": None,
+    "image_file": image_file,
+    "sep": ",",
+    "temperature": 0,
+    "top_p": None,
+    "num_beams": 1,
+    "max_new_tokens": 4096
+})()
+
+eval_model(args)
